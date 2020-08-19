fb569df83a83b5b4b5ff2bd9b56ea308c8c3e6ca
vega2k <vega2k@naver.com>
2020-08-19 15:10:08 +0900

    venv add, djongo 연결

--- mydjango/settings.py
+++ mydjango/settings.py
@@ -82,14 +82,22 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+    # 'default': {
+    #      'ENGINE': 'django.db.backends.mysql',
+    #      'NAME': 'django_db', # DB명
+    #      'USER': 'python', # 데이터베이스 계정
+    #      'PASSWORD':'python', # 계정 비밀번호
+    #      'HOST':'localhost', # 데이테베이스 IP
+    #      'PORT':'3306', # 데이터베이스 port
+    #  }
     'default': {
-         'ENGINE': 'django.db.backends.mysql',
-         'NAME': 'django_db', # DB명
-         'USER': 'python', # 데이터베이스 계정
-         'PASSWORD':'python', # 계정 비밀번호
-         'HOST':'localhost', # 데이테베이스 IP
-         'PORT':'3306', # 데이터베이스 port
-     }
+        'ENGINE': 'djongo',
+        'NAME': 'django_db',  # DB명
+        'USER': 'python',  # 데이터베이스 계정
+        'PASSWORD': 'python',  # 계정 비밀번호
+        'HOST': 'localhost',  # 데이테베이스 IP
+        'PORT': '27017',  # 데이터베이스 port
+    }
 }
 
 
